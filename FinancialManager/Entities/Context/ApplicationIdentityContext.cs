311b12440a0258cc99de815e3f1fea0c1cd37598
Nero <nero@engegraph.com.br>
2015-11-18 17:59:12 -0200

    Owin Implementation

---
+++ FinancialManager/Entities/Context/ApplicationIdentityContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManager.Entities.Context
+{
+    public class ApplicationIdentityContext : IdentityDbContext
+    {
+        public ApplicationIdentityContext(String pConnectionString)
+            :base(pConnectionString)
+        {
+
+        }
+    }
+}
